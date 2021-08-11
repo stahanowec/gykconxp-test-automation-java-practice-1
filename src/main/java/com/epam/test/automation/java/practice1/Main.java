cb0b22e8be4fc2cdec80a85e3b16e269e81c36f0
Hurin Serhiy <sgurin@ukr.net>
2021-08-11 16:49:42 +0300

    TASK1 && TASK2

--- src/main/java/com/epam/test/automation/java/practice1/Main.java
+++ src/main/java/com/epam/test/automation/java/practice1/Main.java
@@ -2,24 +2,38 @@ package com.epam.test.automation.java.practice1;
 
 public class Main {
 
-    /**
-     * <summary>
-     * Implement code according to description of task 1.
-     * </summary>
-     */
+    private static final int DEFAULT_SQUARE = 2;
+    private static final int ALLOWED_SIZE = 3;
+
     public static int task1(int n) {
-        //TODO: Delete line below and write your own solution;
-        throw new UnsupportedOperationException();
+        return (n >= 0) ? (int) Math.pow(n, DEFAULT_SQUARE) : -n;
     }
 
-    /**
-     * <summary>
-     * Implement code according to description of task 2.
-     * </summary>
-     */
+
     public static int task2(int n) {
-        //TODO: Delete line below and write your own solution;
-        throw new UnsupportedOperationException();
+
+        int count[] = new int[10];
+
+        var str = Integer.toString(n);
+        if (str.length() != ALLOWED_SIZE) {
+            throw new UnsupportedOperationException("Invalid value");
+        }
+        for (int i = 0; i < str.length(); i++) {
+
+            count[str.charAt(i) - '0']++;
+        }
+
+        int result = 0, multiplier = 1;
+
+        for (int i = 0; i <= 9; i++) {
+            while (count[i] > 0) {
+                result = result + (i * multiplier);
+                count[i]--;
+                multiplier = multiplier * 10;
+            }
+        }
+
+        return result;
     }
 
 }
--- src/test/java/com/epam/test/automation/java/practice1/MainTest.java
+++ src/test/java/com/epam/test/automation/java/practice1/MainTest.java
@@ -1,5 +1,49 @@
 package com.epam.test.automation.java.practice1;
 
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.testng.Assert.assertThrows;
+
+
 public class MainTest {
 
+    @Test(dataProvider = "task1DataProvider")
+    public void ifTask1ThenSuccess(int currentValue, int expectedValue) {
+        //given
+        var current = currentValue;
+        var expected = expectedValue;
+        //when
+        var result = Main.task1(current);
+        //then
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void ifTask2ThenSuccess() {
+        //given
+        var current = 165;
+        var expected = 651;
+        //when
+        var result = Main.task2(current);
+        //then
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void ifTask2ThenThrowsUnsupportedOperation() {
+        //given
+        var current = 99;
+        //when
+        //then
+        assertThrows(UnsupportedOperationException.class, () -> Main.task2(current));
+
+    }
+
+    @DataProvider(name = "task1DataProvider")
+    public static Object[][] task1DataProvider() {
+        return new Object[][]{{4, 16}, {-5, 5}, {0, 0}};
+    }
 }
