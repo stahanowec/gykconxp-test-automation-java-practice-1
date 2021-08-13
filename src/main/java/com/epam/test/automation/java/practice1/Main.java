d6a8bf466d985d3685cab55325d40a188e070711
Hurin Serhiy <sgurin@ukr.net>
2021-08-13 20:22:58 +0300

    TASK1 && TASK2 fix code smells

--- src/main/java/com/epam/test/automation/java/practice1/Main.java
+++ src/main/java/com/epam/test/automation/java/practice1/Main.java
@@ -1,31 +1,32 @@
 package com.epam.test.automation.java.practice1;
 
+import java.util.stream.IntStream;
+
 public class Main {
 
     private static final int DEFAULT_SQUARE = 2;
     private static final int ALLOWED_SIZE = 3;
 
-    public static int task1(int n) {
-        return (n >= 0) ? (int) Math.pow(n, DEFAULT_SQUARE) : -n;
-    }
+    private Main() {}
 
+    public static int task1(int n) {
+        return (n >= 0) ? (int) Math.pow(n, DEFAULT_SQUARE) : Math.abs(n);
+    }
 
     public static int task2(int n) {
 
-        int count[] = new int[10];
+        var count = new int[10];
 
         var str = Integer.toString(n);
         if (str.length() != ALLOWED_SIZE) {
             throw new UnsupportedOperationException("Invalid value");
         }
-        for (int i = 0; i < str.length(); i++) {
+        IntStream.range(0, str.length()).forEach(i -> count[str.charAt(i) - '0']++);
 
-            count[str.charAt(i) - '0']++;
-        }
+        var result = 0;
+        var multiplier = 1;
 
-        int result = 0, multiplier = 1;
-
-        for (int i = 0; i <= 9; i++) {
+        for (var i = 0; i <= 9; i++) {
             while (count[i] > 0) {
                 result = result + (i * multiplier);
                 count[i]--;
